38d23c5e2e18d4dacbbb9604cd3664bd8063b029
Gabriel Oliveira <greewdyBlu@gmail.com>
2021-02-16 00:09:24 -0300

    atualizacoes de layout ver2

--- app/view/Tela_Principal/index.php
+++ app/view/Tela_Principal/index.php
@@ -12,7 +12,7 @@
    <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@600&display=swap" rel="stylesheet">  
   
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
 </head>
 <body>
